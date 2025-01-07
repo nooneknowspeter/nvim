b5b4afecc9290587dda9ce510117ee4a8d4d4c4c
nooneknowspeter <nooneknowspeter@users.noreply.github.com>
2025-01-07 09:07:54 -0400

    arranged plugins in order

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,95 +1,11 @@
 return {
-  -- conform
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  -- neovim lsp
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  -- nvim treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = "VeryLazy",
-    opts = require "configs.treesitter",
-  },
-
-  -- multicursors
-  {
-    "smoka7/multicursors.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvimtools/hydra.nvim",
-    },
-    opts = {},
-    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-    keys = {
-      {
-        mode = { "v", "n" },
-        "<Leader>m",
-        "<cmd>MCstart<cr>",
-        desc = "Create a selection for selected text or word under the cursor",
-      },
-    },
-  },
-
-  -- escape using key combo (currently set to jk)
+  -- better escape; escape using key combo (currently set to jk)
   {
     "max397574/better-escape.nvim",
     config = function()
       require("better_escape").setup()
     end,
     lazy = false,
-  },
-
-  -- plenery
-  {
-    "nvim-lua/plenary.nvim",
-  },
-
-  -- json parser
-  {
-    "Joakker/lua-json5",
-    event = "VeryLazy",
-    lazy = true,
-    build = vim.fn.has "win32" == 1 and "powershell ./install.ps1" or "./install.sh",
-  },
-
-  -- nvim dap
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-      "theHamsta/nvim-dap-virtual-text",
-      "jay-babu/mason-nvim-dap.nvim",
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      require "configs.dap"
-    end,
-  },
-
-  -- nvim dap python
-  {
-    "mfussenegger/nvim-dap-python",
-    event = "VeryLazy",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      local mason_registry = require "mason-registry"
-
-      require("dap-python").setup(mason_registry.get_package("debugpy"):get_install_path() .. "/venv/bin/python")
-    end,
   },
 
   -- cmake tools
@@ -118,6 +34,88 @@ return {
     opts = {},
   },
 
+  -- conform
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- json parser
+  {
+    "Joakker/lua-json5",
+    event = "VeryLazy",
+    lazy = true,
+    build = vim.fn.has "win32" == 1 and "powershell ./install.ps1" or "./install.sh",
+  },
+
+  -- multicursors
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
+
+  -- neovim lsp
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end,
+    lazy = false,
+  },
+
+  -- nvim treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    opts = require "configs.treesitter",
+  },
+
+  -- debugging
+  -- nvim dap, dap ui
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+      "theHamsta/nvim-dap-virtual-text",
+      "jay-babu/mason-nvim-dap.nvim",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require "configs.dap"
+    end,
+  },
+
+  -- nvim python
+  {
+
+    "mfussenegger/nvim-dap-python",
+    event = "VeryLazy",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      local mason_registry = require "mason-registry"
+
+      require("dap-python").setup(mason_registry.get_package("debugpy"):get_install_path() .. "/venv/bin/python")
+    end,
+  },
+
   -- nvim surround
   {
     "kylechui/nvim-surround",
@@ -126,5 +124,10 @@ return {
     config = function()
       require "configs.nvimsurround"
     end,
+  },
+
+  -- plenery
+  {
+    "nvim-lua/plenary.nvim",
   },
 }
