1d2767a278a1d16f63becab6d14a399eba3e4da5
nooneknowspeter <nooneknowspeter@users.noreply.github.com>
2025-01-07 04:56:58 -0400

    added more dap
    netcore
    
    lldb
    
    gdb
    
    js-debugger

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -1,6 +1,10 @@
+local mason = require "mason"
 local mason_dap = require "mason-nvim-dap"
 local dap = require "dap"
 local dapui = require "dapui"
+local mason_registry = require "mason-registry"
+
+mason.setup()
 
 mason_dap.setup {
   ensure_installed = { "python", "node2", "chrome", "js", "codelldb", "bash" },
@@ -23,3 +27,38 @@ end
 
 -- breakpoint icon
 vim.fn.sign_define("DapBreakpoint", { text = "👾" })
+
+-- codelldb
+dap.adapters.lldb = {
+  type = "executable",
+  command = mason_registry.get_package("codelldb"):get_install_path() .. "/codelldb", -- adjust as needed, must be absolute path
+  name = "lldb",
+}
+
+-- gdb
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+}
+
+-- js debug adapter
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    args = {
+      mason_registry.get_package("js-debug-adapter"):get_install_path() .. "/js-debug/src/dapDebugServer.js",
+      "${port}",
+    },
+  },
+}
+
+-- netcore db
+dap.adapters.coreclr = {
+  type = "executable",
+  command = mason_registry.get_package("netcoredbg"):get_install_path() .. "/netcoredbg",
+  args = { "--interpreter=vscode" },
+}
