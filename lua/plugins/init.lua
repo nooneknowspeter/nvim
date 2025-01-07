a947c753110b8c0baa5a697e339f09fafdc30e16
peter <nooneknowspeter@users.noreply.github.com>
2025-01-06 22:19:11 -0400

    added nvim-dap with auto config with daps from mason

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,9 +67,29 @@ return {
       "jay-babu/mason-nvim-dap.nvim",
     },
     config = function()
-      require "configs.dap"
+      require("mason-nvim-dap").setup {
+        automatic_config = true, -- automatically configure based on mason-installed daps
+        project_based_config = true, -- enable project-based configurations
+      }
+
+      -- optionally, set up language-specific configurations based on project context
+      -- (e.g., from .vscode directory or other project files)
+      require("dap").configurations = function()
+        local project_dap_config = vim.fn.findfile(".vscode/launch.json", vim.fn.expand "%:p:h")
+        if project_dap_config ~= "" then
+          -- load the project-specific config
+          local config = vim.fn.json_decode(vim.fn.readfile(project_dap_config))
+          return config
+        else
+          -- default or global config
+          return {
+            -- default configurations for common languages, etc.
+          }
+        end
+      end
     end,
   },
+
   -- cmake tools
   {
     "Civitasv/cmake-tools.nvim",
