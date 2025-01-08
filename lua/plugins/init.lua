3c621b83fc417f78072061f53d7344492bfd144a
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-08 11:27:50 -0400

    added dadbod and dadbod ui for databases

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,6 +39,24 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
 
+  -- dadbod; database tools
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+
   -- markdown previewer
   {
     "iamcco/markdown-preview.nvim",
