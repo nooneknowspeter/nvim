2b7e97ed4aeabc6f7af7280631b2ede737fa1f9c
peter <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-07 23:51:23 -0400

    Update conform.lua
    dynamically setups up formatters installed from mason

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,22 +1,20 @@
-local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "eslint_d", "prettierd" },
-    html = { "eslint_d", "prettierd" },
-    javascriptreact = { "eslint_d", "prettierd" },
-    typescriptreact = { "eslint_d", "prettierd" },
-    javascript = { "eslint_d", "prettierd" },
-    typescript = { "eslint_d", "prettierd" },
-    json = { "eslint_d", "prettierd" },
-    yaml = { "eslint_d", "prettierd" },
-    cs = { "csharpier" },
-    cpp = { "clang-format" },
-    c = { "clang-format" },
-    python = { "isort", "black" },
-    graphql = { "pretterd" },
-    md = { "eslint_d", "pretterd" },
-  },
+local mason_registry = require "mason-registry"
 
+-- initialize the formatters_by_ft table dynamically
+local formatters_by_ft = {}
+
+-- use mason's metadata to include all available formatters
+for _, package in ipairs(mason_registry.get_installed_packages()) do
+  if package.spec.categories[1] == "formatter" then
+    for _, ft in ipairs(package.spec.filetypes or {}) do
+      formatters_by_ft[ft] = formatters_by_ft[ft] or {}
+      table.insert(formatters_by_ft[ft], package.name)
+    end
+  end
+end
+
+local options = {
+  formatters_by_ft = formatters_by_ft,
   format_on_save = {
     -- these options will be passed to conform.format()
     timeout_ms = 500,
