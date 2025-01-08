ca9a6158d4d27f397f846ab2ceafa3667e72f5a1
peter <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-08 00:09:35 -0400

    Reverted lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,20 +1,31 @@
--- load defaults i.e., lua_lsp
+-- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-local mason_registry = require "mason-registry"
+
+-- EXAMPLE
+local servers = {
+  "html",
+  "cssls",
+  "csharp_ls",
+  "ts_ls",
+  "cmake",
+  "clangd",
+  "tailwindcss",
+  "sqlls",
+  "pyright",
+  "postgres_lsp",
+  "nginx_language_server",
+  "jsonls",
+  "graphql",
+  "glsl_analyzer",
+  "dockerls",
+  "marksman",
+}
+
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- get the list of all installed lsps from mason
-local servers = {}
-
-for _, package in ipairs(mason_registry.get_installed_packages()) do
-  if package.spec.categories[1] == "lsp" then
-    table.insert(servers, package.name)
-  end
-end
-
--- configure each lsp
+-- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
