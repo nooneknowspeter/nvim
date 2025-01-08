345aec9f2a235f8c1a6dd5e1281903d54f3d6863
peter <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-07 23:50:49 -0400

    Update lspconfig.lua
    automatically loads language servers installed from mason

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,31 +1,20 @@
--- load defaults i.e lua_lsp
+-- load defaults i.e., lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = {
-  "html",
-  "cssls",
-  "csharp_ls",
-  "ts_ls",
-  "cmake",
-  "clangd",
-  "tailwindcss",
-  "sqlls",
-  "pyright",
-  "postgres_lsp",
-  "nginx_language_server",
-  "jsonls",
-  "graphql",
-  "glsl_analyzer",
-  "dockerls",
-  "marksman",
-}
-
+local mason_registry = require "mason-registry"
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+-- get the list of all installed lsps from mason
+local servers = {}
+
+for _, package in ipairs(mason_registry.get_installed_packages()) do
+  if package.spec.categories[1] == "lsp" then
+    table.insert(servers, package.name)
+  end
+end
+
+-- configure each lsp
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
