bf9edc0de122048ed06b61a85be303bbde163e94
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-18 16:48:50 -0400

    tried telescope media previewer

--- lua/configs/notify.lua
+++ lua/configs/notify.lua
@@ -1,12 +1,12 @@
 local notify = require "notify"
 
 notify.setup {
-  stages = "fade", -- animation style for notifications
-  timeout = 3000, -- default timeout for notifications
+  stages = "fade",               -- animation style for notifications
+  timeout = 3000,                -- default timeout for notifications
   background_colour = "#FFFFFF", -- customize the background color
-  render = "wrapped-compact", -- render method, options: minimal, compact, etc.
-  max_width = 64, -- maximum width of notifications
-  max_height = 32, -- maximum height of notifications
+  render = "wrapped-compact",    -- render method, options: minimal, compact, etc.
+  max_width = 32,                -- maximum width of notifications
+  max_height = 16,               -- maximum height of notifications
   top_down = true,
 }
 
