f54371bacd42e4ae7a1253d94b41029c7abfc60e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-26 13:07:57 -0400

    got vimtex live preview to work with zathura

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -3,9 +3,11 @@ return {
   {
     "lervag/vimtex",
     enabled = true,
-    lazy = false,                                         -- lazy-loading will disable inverse search
+    lazy = false, -- lazy-loading will disable inverse search
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable "pplatex" == 1 and "pplatex" or "latexlog"
+      vim.g.vimtex_view_method = "zathura"
     end,
     keys = {
       { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
