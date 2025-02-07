2d5da28249ea9e4082fb5e57563196cdcfdc313e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-07 17:23:00 -0400

    updated configs for telescope

--- lua/configs/telescope-themes.lua
+++ lua/configs/telescope-themes.lua
@@ -2,65 +2,64 @@
 local builtin_schemes = require("telescope._extensions.themes").builtin_schemes
 
 require("telescope").setup({
-  extensions = {
-    themes = {
-      -- you can add regular telescope config
-      -- that you want to apply on this picker only
-      layout_config = {
-        horizontal = {
-          width = 0.8,
-          height = 0.7,
-        },
-      },
+	extensions = {
+		themes = {
+			-- you can add regular telescope config
+			-- that you want to apply on this picker only
+			layout_config = {
+				horizontal = {
+					width = 0.8,
+					height = 0.7,
+				},
+			},
 
-      -- extension specific config
+			-- extension specific config
 
-      -- (boolean) -> show/hide previewer window
-      enable_previewer = true,
+			-- (boolean) -> show/hide previewer window
+			enable_previewer = true,
 
-      -- (boolean) -> enable/disable live preview
-      enable_live_preview = true,
+			-- (boolean) -> enable/disable live preview
+			enable_live_preview = true,
 
-      -- all builtin themes are ignored by default
-      -- (list) -> provide table of theme names to overwrite builtins list
+			-- all builtin themes are ignored by default
+			-- (list) -> provide table of theme names to overwrite builtins list
 
-      -- ignore = { "default", "desert", "elflord", "habamax" },
+			-- ignore = { "default", "desert", "elflord", "habamax" },
+			-- OR
+			-- extend the required `builtin_schemes` list to ignore other
+			-- schemes in addition to builtin schemes
+			-- ignore = vim.list_extend(builtin_schemes, { "embark" }),
 
-      -- OR
-      -- extend the required `builtin_schemes` list to ignore other
-      -- schemes in addition to builtin schemes
-      -- ignore = vim.list_extend(builtin_schemes, { "embark" }),
+			-- (table)
 
-      -- (table)
+			-- (boolean) ignore -> toggle ignore light themes
+			-- (list) keywords -> list of keywords that would identify as light theme
+			-- light_themes = {
+			-- 	ignore = true,
+			--
+			-- 	keywords = { "light", "day", "frappe" },
+			-- },
 
-      -- (boolean) ignore -> toggle ignore light themes
-      -- (list) keywords -> list of keywords that would identify as light theme
-      -- light_themes = {
-      -- 	ignore = true,
-      --
-      -- 	keywords = { "light", "day", "frappe" },
-      -- },
+			-- (table)
+			-- (boolean) ignore -> toggle ignore dark themes
+			-- (list) keywords -> list of keywords that would identify as dark theme
+			-- dark_themes = {
+			-- 	ignore = false,
+			--
+			-- 	keywords = { "dark", "night", "black" },
+			-- },
 
-      -- (table)
-      -- (boolean) ignore -> toggle ignore dark themes
-      -- (list) keywords -> list of keywords that would identify as dark theme
-      -- dark_themes = {
-      -- 	ignore = false,
-      --
-      -- 	keywords = { "dark", "night", "black" },
-      -- },
+			persist = {
+				-- enable persisting last theme choice
+				enabled = true,
+			},
 
-      persist = {
-        -- enable persisting last theme choice
-        enabled = true,
-      },
-
-      mappings = {
-        -- for people used to other mappings
-        down = "<C-n>",
-        up = "<C-p>",
-        accept = "<C-y>",
-      },
-    },
-  },
+			mappings = {
+				-- for people used to other mappings
+				down = "<C-n>",
+				up = "<C-p>",
+				accept = "<C-y>",
+			},
+		},
+	},
 })
