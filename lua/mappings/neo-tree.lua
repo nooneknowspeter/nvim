227529bcf9c96654cdb6152456fab4f80cb9df7f
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-07 17:22:50 -0400

    updated mappings for neotree

--- lua/mappings/neo-tree.lua
+++ lua/mappings/neo-tree.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
 local del = vim.keymap.del
-map("n", "<C-n>", ":Neotree toggle<CR>", { desc = "neo-tree toggle window" })
+map("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "neo-tree toggle window" })
 
-map("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { desc = "neo-tree reveal float" })
+map("n", "<leader>bf", "<cmd>Neotree buffers reveal float<CR>", { desc = "neo-tree reveal float" })
