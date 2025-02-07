f63e3c40efdbbfabcef83487ee949c7d9deb1d28
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-07 03:28:02 -0400

    updated mason lspconfig

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,19 +1,11 @@
-require("mason").setup()
-require("mason-lspconfig").setup()
-local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-require("mason-lspconfig").setup_handlers {
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
-  function(server_name)    -- default handler (optional)
-    require("lspconfig")[server_name].setup {
-      capabilities = capabilities
-    }
-  end,
-  -- Next, you can provide a dedicated handler for specific servers.
-  -- For example, a handler override for the `rust_analyzer`:
-  ["rust_analyzer"] = function()
-    require("rust-tools").setup {}
-  end
-}
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	-- Replace the language servers listed here
+	-- with the ones you want to install
+	ensure_installed = { "lua_ls", "rust_analyzer" },
+	handlers = {
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
+	},
+})
