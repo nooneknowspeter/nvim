8513afcc1372ff6d794c464a6ed93bc213f14d22
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-07 04:24:37 -0400

    added lualine transparent theme

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -1,6 +1,53 @@
-require('lualine').setup {
+require("lualine").setup({
   options = {
     icons_enabled = true,
+    theme = function()
+      local colors = {
+        darkgray = "#16161d",
+        gray = "#727169",
+        innerbg = nil,
+        outerbg = "#16161D",
+        normal = "#7e9cd8",
+        insert = "#98bb6c",
+        visual = "#ffa066",
+        replace = "#e46876",
+        command = "#e6c384",
+      }
+      return {
+        inactive = {
+          a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+          b = { fg = colors.gray, bg = colors.outerbg },
+          c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        visual = {
+          a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+          b = { fg = colors.gray, bg = colors.outerbg },
+          c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        replace = {
+          a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+          b = { fg = colors.gray, bg = colors.outerbg },
+          c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        normal = {
+          a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+          b = { fg = colors.gray, bg = colors.outerbg },
+          c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        insert = {
+          a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+          b = { fg = colors.gray, bg = colors.outerbg },
+          c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        command = {
+          a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+          b = { fg = colors.gray, bg = colors.outerbg },
+          c = { fg = colors.gray, bg = colors.innerbg },
+        },
+      }
+    end,
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -13,28 +60,27 @@ require('lualine').setup {
       statusline = 100,
       tabline = 100,
       winbar = 100,
-    }
+    },
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
-
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
   },
 
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
   },
   tabline = {},
   winbar = {},
   inactive_winbar = {},
-  extensions = {}
-}
+  extensions = {},
+})
