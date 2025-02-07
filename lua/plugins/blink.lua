fe3a159f34ce34aa8c72dcf7f8ab84fcd4917bd1
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-07 17:23:23 -0400

    updated blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,30 +1,31 @@
 return {
-	{
-		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			"niuiic/blink-cmp-rg.nvim",
-			-- add blink.compat to dependencies
-			{
-				"saghen/blink.compat",
-				optional = true, -- make optional so it's only enabled if any extras need it
-				opts = {},
-			},
-		},
+  {
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+      'Kaiser-Yang/blink-cmp-dictionary',
+      "rafamadriz/friendly-snippets",
+      "niuiic/blink-cmp-rg.nvim",
+      -- add blink.compat to dependencies
+      {
+        "saghen/blink.compat",
+        optional = true, -- make optional so it's only enabled if any extras need it
+        opts = {},
+      },
+    },
 
-		-- use a release tag to download pre-built binaries
-		version = "*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
+    -- use a release tag to download pre-built binaries
+    version = "*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = require("configs.blink"),
-		opts_extend = {
-			"sources.default",
-		},
-	},
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = require("configs.blink"),
+    opts_extend = {
+      "sources.default",
+    },
+  },
 }
