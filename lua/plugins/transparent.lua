ee7c9730c428a598775e8fc672110fbb7f0fd0c5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 13:00:36 -0400

    updated transparent config

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,45 +1,46 @@
 return {
-  {
-    "xiyaowong/transparent.nvim",
-    lazy = false,                 --this was SUPER IMPORTANT
-    config = function()
-      require("transparent").setup({ -- Optional, you don't have to run setup.
-        groups = {                -- table: default groups
-          "Normal",
-          "NormalNC",
-          "Comment",
-          "Constant",
-          "Special",
-          "Identifier",
-          "Statement",
-          "PreProc",
-          "Type",
-          "Underlined",
-          "Todo",
-          "String",
-          "Function",
-          "Conditional",
-          "Repeat",
-          "Operator",
-          "Structure",
-          "LineNr",
-          "NonText",
-          "SignColumn",
-          "CursorLine",
-          "CursorLineNr",
-          "StatusLine",
-          "StatusLineNC",
-          "EndOfBuffer",
-        },
-        extra_groups = {
-          "NeoTreeNormal",
-          "NeoTreeNormalNC",
-          "BufferLine",
-          "lualine",
-          "Telescope",
-        },               -- and this was super important as well
-        exclude_groups = {}, -- table: groups you don't want to clear
-      })
-    end,
-  },
+	{
+		"xiyaowong/transparent.nvim",
+		lazy = false, --this was SUPER IMPORTANT
+		config = function()
+			require("transparent").setup({ -- Optional, you don't have to run setup.
+				groups = { -- table: default groups
+					"Normal",
+					"NormalNC",
+					"Comment",
+					"Constant",
+					"Special",
+					"Identifier",
+					"Statement",
+					"PreProc",
+					"Type",
+					"Underlined",
+					"Todo",
+					"String",
+					"Function",
+					"Conditional",
+					"Repeat",
+					"Operator",
+					"Structure",
+					"LineNr",
+					"NonText",
+					"SignColumn",
+					"CursorLine",
+					"CursorLineNr",
+					"StatusLine",
+					"StatusLineNC",
+					"EndOfBuffer",
+				},
+				extra_groups = {
+					"NeoTreeNormal",
+					"NeoTreeNormalNC",
+					"BufferLine",
+					"lualine",
+					"Telescope",
+					"Lazy",
+				}, -- and this was super important as well
+				exclude_groups = {}, -- table: groups you don't want to clear
+			})
+		end,
+	},
 }
