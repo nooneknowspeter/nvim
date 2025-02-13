d6849b5caa1ef1cac8ddb6fd61b0c386002e192b
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-12 21:01:49 -0400

    added CCC for color picking

---
+++ lua/plugins/ccc.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "uga-rosa/ccc.nvim",
+    enabled = true,
+    config = function()
+      local ccc = require("ccc")
+      local mapping = ccc.mapping
+
+      ccc.setup({
+        -- Your preferred settings
+        -- Example: enable highlighter
+        highlighter = {
+          auto_enable = true,
+          lsp = true,
+        },
+      })
+    end
+  }
+
+}
