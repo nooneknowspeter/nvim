a650b8d728490bce50b790f53f960d51e82d29c0
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-03-03 16:04:40 -0400

    remapped better escape to just jj

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,11 +1,43 @@
 return {
-  -- better escape; escape using key combo (currently set to jk)
-  {
-    "max397574/better-escape.nvim",
-    enabled = true,
-    config = function()
-      require("better_escape").setup()
-    end,
-    lazy = false,
-  },
+	-- better escape; escape using key combo (currently set to jk)
+	{
+		"max397574/better-escape.nvim",
+		enabled = true,
+		config = function()
+			-- lua, default settings
+			require("better_escape").setup({
+				timeout = vim.o.timeoutlen,
+				default_mappings = false,
+				mappings = {
+					i = {
+						j = {
+							-- These can all also be functions
+							j = "<Esc>",
+						},
+					},
+					c = {
+						j = {
+							j = "<Esc>",
+						},
+					},
+					t = {
+						j = {
+							j = "<C-\\><C-n>",
+						},
+					},
+					v = {
+						j = {
+							j = "<Esc>",
+						},
+					},
+					s = {
+						j = {
+							j = "<Esc>",
+						},
+					},
+				},
+			})
+		end,
+		lazy = false,
+	},
 }
