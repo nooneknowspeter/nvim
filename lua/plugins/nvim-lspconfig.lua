47c78c147f563dad820e8bd9f7c4bbc0133369b5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-04-04 19:52:21 -0400

    changed plugin urls and branches for development and testing

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,30 +1,34 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		enabled = true,
-		cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-		opts = function()
-			return require("configs.mason")
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    enabled = true,
+    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+    opts = function()
+      return require("configs.mason")
+    end,
+  },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		enabled = true,
-		dependencies = { "williamboman/mason.nvim" },
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("configs.mason-lspconfig")
-		end,
-	},
+  {
+    -- "williamboman/mason-lspconfig.nvim", -- main repo
+    "nooneknowspeter/mason-lspconfig.nvim", -- forked repo (testing)
+    branch = "feat/fountain-lsp-server",
+    enabled = true,
+    dependencies = { "williamboman/mason.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("configs.mason-lspconfig")
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		enabled = true,
-		dependencies = { "saghen/blink.cmp" },
-		lazy = false,
-		config = function()
-			require("configs.nvim-lspconfig")
-		end,
-	},
+  {
+    -- "neovim/nvim-lspconfig", -- main repo
+    "nooneknowspeter/nvim-lspconfig", -- forked repo (testing)
+    branch = "feat/fountain-lsp-server",
+    enabled = true,
+    dependencies = { "saghen/blink.cmp" },
+    lazy = false,
+    config = function()
+      require("configs.nvim-lspconfig")
+    end,
+  },
 }
