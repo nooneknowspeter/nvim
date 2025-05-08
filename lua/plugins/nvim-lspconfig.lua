a1995159aa09778d4f6f7c5aa39a928054774126
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-05-08 12:05:52 -0400

    change urls for mason and set fixed version to 1.11.0

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,34 +1,35 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    enabled = true,
-    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-    opts = function()
-      return require("configs.mason")
-    end,
-  },
+	{
+		"mason-org/mason.nvim",
+		enabled = true,
+		version = "1.11.0",
+		cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+		opts = function()
+			return require("configs.mason")
+		end,
+	},
 
-  {
-    -- "williamboman/mason-lspconfig.nvim", -- main repo
-    "nooneknowspeter/mason-lspconfig.nvim", -- forked repo (testing)
-    branch = "feat/fountain-lsp-server",
-    enabled = true,
-    dependencies = { "williamboman/mason.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("configs.mason-lspconfig")
-    end,
-  },
+	{
+		-- "williamboman/mason-lspconfig.nvim", -- main repo
+		"nooneknowspeter/mason-lspconfig.nvim", -- forked repo (testing)
+		branch = "feat/fountain-lsp-server",
+		enabled = true,
+		dependencies = { { "mason-org/mason.nvim", version = "1.11.0" } },
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("configs.mason-lspconfig")
+		end,
+	},
 
-  {
-    -- "neovim/nvim-lspconfig", -- main repo
-    "nooneknowspeter/nvim-lspconfig", -- forked repo (testing)
-    branch = "feat/fountain-lsp-server",
-    enabled = true,
-    dependencies = { "saghen/blink.cmp" },
-    lazy = false,
-    config = function()
-      require("configs.nvim-lspconfig")
-    end,
-  },
+	{
+		-- "neovim/nvim-lspconfig", -- main repo
+		"nooneknowspeter/nvim-lspconfig", -- forked repo (testing)
+		branch = "feat/fountain-lsp-server",
+		enabled = true,
+		dependencies = { "saghen/blink.cmp" },
+		lazy = false,
+		config = function()
+			require("configs.nvim-lspconfig")
+		end,
+	},
 }
