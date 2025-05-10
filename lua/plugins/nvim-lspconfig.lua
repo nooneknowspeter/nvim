32e627e24de96ed5d23f30ba2aa3004a608b8f64
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-05-09 22:20:02 -0400

    changed urls to official lsp repos

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -10,10 +10,11 @@ return {
 	},
 
 	{
-		-- "williamboman/mason-lspconfig.nvim", -- main repo
-		"nooneknowspeter/mason-lspconfig.nvim", -- forked repo (testing)
-		branch = "feat/fountain-lsp-server",
+		"mason-org/mason-lspconfig.nvim", -- main repo
+		-- "nooneknowspeter/mason-lspconfig.nvim", -- forked repo (testing)
+		-- branch = "feat/fountain-lsp-server",
 		enabled = true,
+		version = "1.32.0",
 		dependencies = { { "mason-org/mason.nvim", version = "1.11.0" } },
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
@@ -22,14 +23,14 @@ return {
 	},
 
 	{
-		-- "neovim/nvim-lspconfig", -- main repo
-		"nooneknowspeter/nvim-lspconfig", -- forked repo (testing)
-		branch = "feat/fountain-lsp-server",
+		"neovim/nvim-lspconfig", -- main repo
+		-- 	"nooneknowspeter/nvim-lspconfig", -- forked repo (testing)
+		-- 	branch = "feat/fountain-lsp-server",
 		enabled = true,
 		dependencies = { "saghen/blink.cmp" },
 		lazy = false,
-		config = function()
-			require("configs.nvim-lspconfig")
-		end,
+		-- config = function()
+		--   require("configs.nvim-lspconfig")
+		-- end,
 	},
 }
