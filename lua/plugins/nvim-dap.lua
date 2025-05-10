e6001108b5eb6fbfb2d617e4f7454108b8a603dd
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-05-09 22:19:47 -0400

    updated urls for nvim dap repos

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,20 +1,21 @@
 return {
-  -- debugging; nvim dap, dap ui
-  {
-    "rcarriga/nvim-dap-ui",
-    enabled = true,
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-      "theHamsta/nvim-dap-virtual-text",
-      { "nooneknowspeter/mason-nvim-dap.nvim", branch = "feat/additional-js-dap-configs" },
-      "nvim-neotest/nvim-nio",
+	-- debugging; nvim dap, dap ui
+	{
+		"rcarriga/nvim-dap-ui",
+		enabled = true,
+		event = "VeryLazy",
+		dependencies = {
+			{ "mason-org/mason.nvim", version = "1.11.0" },
+			"mfussenegger/nvim-dap",
+			"theHamsta/nvim-dap-virtual-text",
+			-- { "nooneknowspeter/mason-nvim-dap.nvim", branch = "feat/additional-js-dap-configs" },
+			"jay-babu/mason-nvim-dap.nvim",
+			"nvim-neotest/nvim-nio",
 
-      "jbyuki/one-small-step-for-vimkind",
-    },
-    config = function()
-      require "configs.nvim-dap"
-    end,
-  },
+			"jbyuki/one-small-step-for-vimkind",
+		},
+		config = function()
+			require("configs.nvim-dap")
+		end,
+	},
 }
