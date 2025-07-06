de6367b52096bb43d50a9fec6ae69a463e5dc17d
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-06 17:33:28 -0400

    refactor: updated nvim-lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,7 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		enabled = true,
-		dependencies = { "saghen/blink.cmp" },
-		lazy = false,
-		config = function()
-			require("configs.nvim-lspconfig")
-		end,
-	},
+  {
+    "neovim/nvim-lspconfig",
+    enabled = true,
+    dependencies = { "saghen/blink.cmp" },
+  },
 }
