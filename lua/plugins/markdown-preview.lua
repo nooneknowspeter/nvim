c605901175aad409dfb4bfc93fe740fb2794106a
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-07 05:50:01 -0400

    refactor(plugin/markdown-preview.nvim): changed packaged manager to npm for building markdown-preview.nvim

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,11 +1,8 @@
 return {
-  -- markdown previewer
   {
     "iamcco/markdown-preview.nvim",
-    enabled = true,
-
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
+    build = "cd app && npm install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
