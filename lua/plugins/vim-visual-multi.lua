22b005a80c5776a7daaf2df6d38261067176be7c
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-08 19:25:19 -0400

    refactor(plugin/vim-visual-multi): disabled

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,13 +1,13 @@
 return {
-	{
-		"mg979/vim-visual-multi",
-		enabled = true,
-		lazy = false,
-		init = function()
-			vim.g.VM_maps = {
-				["Find Under"] = "<C-m>",
-				["Find Subword Under"] = "<C-m>",
-			}
-		end,
-	},
+  {
+    "mg979/vim-visual-multi",
+    enabled = false,
+    lazy = false,
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-m>",
+        ["Find Subword Under"] = "<C-m>",
+      }
+    end,
+  },
 }
