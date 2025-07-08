003c20e98c2ad1e1646940471c2dbd7e88879575
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-08 19:22:45 -0400

    feat(dap): added dap support for vscode launch.json

--- lua/configs/nvim-dap.lua
+++ lua/configs/nvim-dap.lua
@@ -1,23 +1,29 @@
 local dap = require("dap")
 local dapui = require("dapui")
+local vscode = require("dap.ext.vscode")
+local json = require("plenary.json")
 
 -- dap ui setup
 dapui.setup()
 
 dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
+	dapui.close()
 end
 
 -- breakpoint icons
 vim.fn.sign_define("DapBreakpoint", { text = "👾" })
-
 vim.fn.sign_define("DapStopped", { text = "💸" })
+
+-- setup dap config by VsCode launch.json file
+vscode.json_decode = function(str)
+	return vim.json.decode(json.json_strip_comments(str))
+end
