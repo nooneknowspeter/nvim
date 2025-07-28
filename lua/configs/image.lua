d6a4b596cb7286e68598d4b699affb8f97730af9
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-27 22:03:20 -0400

    refactor(image.nvim): add additional options to config

--- lua/configs/image.lua
+++ lua/configs/image.lua
@@ -19,4 +19,10 @@ require("image").setup({
   },
   editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
   tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+  max_width = 100,
+  max_height = 12,
+  max_height_window_percentage = math.huge,
+  max_width_window_percentage = math.huge,
+  window_overlap_clear_enabled = true,
+  window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
 })
