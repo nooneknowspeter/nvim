b1535bbe62258d1d3c64f001339b86b88ce0cd32
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-24 12:10:59 -0400

    refactor(zen-mode): add additional options

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -4,6 +4,15 @@ return {
 		opts = {
 			window = {
 				width = 0.90,
+				options = {
+					signcolumn = "yes",
+					number = true,
+					relativenumber = true,
+					cursorline = false,
+					cursorcolumn = false,
+					foldcolumn = "0",
+					list = false,
+				},
 			},
 		},
 	},
